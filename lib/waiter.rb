ec02c0547fa47aa563639ae61c29b5914a9f0da7
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-11 22:53:44 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,9 +1,16 @@
 class Waiter
   attr_accessor :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def all
+    @@all
   end
 
   def new_meal(customer, total, tip=0)
